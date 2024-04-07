6e5c8f8f180e6c910ccbcadd5b728ceb828b4dd7
standardProton <alexm024680@gmail.com>
2024-04-07 01:11:30 -0600

    Added name to footer, updated lookup maps

--- comps/ScheduleFooter.js
+++ comps/ScheduleFooter.js
@@ -6,10 +6,15 @@ export default function ScheduleFooter(){
 
     return (
         <div className={styles.schedule_footer_container}>
-            <div style={{display: "flex", flexWrap: "wrap", gap: "30px", alignItems: "center", justifyContent: "center", margin: "15px"}}>
-                <Link target="_blank" href="mailto:almc8368@colorado.edu">Contact</Link>
-                <Link target="_blank" href="https://paypal.me/c7dev">Donate</Link>
-            </div>
+            <center>
+                <div style={{display: "flex", flexWrap: "wrap", gap: "20px", alignItems: "center", justifyContent: "center", margin: "12px"}}>
+                        <Link target="_blank" href="mailto:almc8368@colorado.edu">Contact</Link>
+                        <Link target="_blank" href="https://paypal.me/c7dev">Donate</Link>
+                </div>
+                <div style={{fontSize: "10pt", color: "#999", paddingBottom: "14px"}}>
+                    <span>Created by Alex McDonald</span>
+                </div>
+            </center>
         </div>
     );
 
